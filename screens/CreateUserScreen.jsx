a4e26ffd45c577d77f8b14ac476e29e5d7b22e67
fernandopaz1 <ferpaz9292@gmail.com>
2021-08-13 19:59:42 -0300

    toFix: los elementos del state no se pueden asignar como valores unicos, son objetos

--- screens/CreateUserScreen.jsx
+++ screens/CreateUserScreen.jsx
@@ -1,33 +1,64 @@
 import React, {useState} from 'react'
 import { View, Button, TextInput, ScrollView, StyleSheet,Text } from 'react-native'
+import firebase from '../database/firebase';
 
 const CreateUserScreen = () => {
     const [state, setState] = useState({
         nombre: '',
         email: '',
-        numeroCel: '',
-
+        numeroCel: ''
     });
 
     const cambiarEstado = (propiedad, valor) => {
         setState({...state, [propiedad]: valor});
     } 
+    console.log(state.nombre)
+    const addNewUser = async () => {
+        console.log(`Nombre ${state.nombre}`)
+        console.log(`email ${state.email}`)
+        console.log(`cel ${state.numeroCel}`)
+        console.log(state)
+        if(state.name === ''){
+            alert('Por favor ingrese un nombre')
+            return
+        }
+        if(state.email === ''){
+            alert('Por favor ingrese un email')
+            return
+        }else if(state.numeroCel === ''){
+            alert('Por favor ingrese un nuemero')
+            return
+        }
+
+        try {
+            await firebase.db.collection('Users').add({
+                name: state.name,
+                email: state.email,
+                phone: state.numeroCel,
+            })
+            // alert('Guardado')
+        } catch (error) {
+            console.log(error)
+        }
+    
+    //    console.log(state)
+    }
 
     return(
         <ScrollView style={styles.container}>
             <View style = {styles.inputGroup}>
-                <TextInput placeholder="Nombre del usuario" onChange={(value) => cambiarEstado("nombre",value)}/>
+                <TextInput placeholder="Nombre del usuario" onChange={(value) => setState({ nombre: value})}/>
             </View>
 
             <View style = {styles.inputGroup}>
-                <TextInput placeholder="Email del usuario"  onChange={(value) => cambiarEstado("email",value)}/>
+                <TextInput placeholder="Email del usuario"  onChange={(value)=>setState({email: value})}/>
             </View>
             <View style = {styles.inputGroup}>
-                <TextInput placeholder="Numero móvil"  onChange={(value) => cambiarEstado("numeroCel",value)}/>
+                <TextInput placeholder="Numero móvil"  onChange={(value) => setState({numeroCel: value})}/>
             </View>
 
             <View>
-                <Button title="Crear usuario" onPress={() => console.log(state)}/>
+                <Button title="Crear usuario" onPress={() => addNewUser()}/>
             </View>
         </ScrollView>
     )
