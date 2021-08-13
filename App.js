39d174e64efaa107b2238a8ad04280ba43b3b7dc
fernandopaz1 <ferpaz9292@gmail.com>
2021-08-13 15:20:06 -0300

    add: paginas de usuarios

--- App.js
+++ App.js
@@ -2,12 +2,40 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// navigation container es un cotenedor de pantallas
+import {NavigationContainer} from '@react-navigation/native'
+
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+
+// esto devuelve un componenete llamado stack
+// este stack es el que contiene a nuestras paginas
+const Stack = createNativeStackNavigator();
+
+
+import UserDetailScreen from './screens/UserDetailScreen';
+import CreateUserScreen from './screens/CreateUserScreen';
+import UserList from './screens/UserList';
+
+// esta funcion es la que va a contener a las multiples pantallas
+function MyStack(){
+  // devuelvo el componente Stack.Navigator y por cada pantalla que quiero agregar 
+  // pongo un Stack.Screen
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="UserList" component={UserList} />
+      <Stack.Screen name="UserDetailScreen" component={UserDetailScreen} />
+      <Stack.Screen name="CreateUserList" component={CreateUserScreen} />
+    </Stack.Navigator>
+
+  )
+}
+
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyStack/>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/UserDetailScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const UserDetailScreen = () => {
+    return(
+        <View>
+            <Text>Users List</Text>
+        </View>
+    )
+}
+
+export default UserDetailScreen
